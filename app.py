94564d46838dedddeac61c0320e65b4059c1fe3b
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-12-23 11:41:39 +0530

    updated

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq   # Groq integration
